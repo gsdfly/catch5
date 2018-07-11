5cbcb18f0ba5eeaa1949ee323eda845a739d487c
lester <lester@catchme.com.cn>
2018-07-11 13:40:22 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -4,9 +4,9 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/sendcoupon6.html'),
+    index: path.resolve(__dirname, '../dist/sendcoupon5.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'sendcoupon6',
+    assetsSubDirectory: 'sendcoupon5',
     assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
