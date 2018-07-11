9b6b9a2604d35918d9418b3a4ccf5a07d01db509
lester <lester@catchme.com.cn>
2018-07-11 13:36:43 +0800

    环形添加401刷新

--- src/api/index.js
+++ src/api/index.js
@@ -27,6 +27,10 @@ instance.interceptors.response.use(function(response) {
   // if(error.response.data.status_code === 401){
   //   await callbackUrl();
   // }
+  if (error.response.data.status_code === 401) {
+    location.reload();
+    return;
+  }
   console.log(error, '全局err')
   store.commit('changeTipContent',{
     imgSrc:"http://res.catchme.com.cn/imgs-2018-02-05/tip/tip1.png",
