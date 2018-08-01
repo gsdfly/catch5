07475a395ed5bcbaf332f98c6d9eaa8dcd5f0bcf
lester <lester@catchme.com.cn>
2018-08-01 15:36:41 +0800

    修改

--- src/components/index2.vue
+++ src/components/index2.vue
@@ -493,6 +493,7 @@
             return;
           }
         }
+        _hmt.push(['_trackEvent','打开优惠券列表弹窗', '点击', '优惠券为：0', '']);
         this.openTip('notExchange');
       },
       receiveTaskGame(){
@@ -559,7 +560,7 @@
         })
       },
       useCoupon(code,end_time) {
-        _hmt.push(['_trackEvent','打开使用优惠券弹窗', '点击', '优惠券为：'+this.activity_bounty[0].voucher_batch.name, '优惠券码为：'+code]);
+        _hmt.push(['_trackEvent','打开使用优惠券弹窗', '点击', '使用优惠券为：'+this.activity_bounty[0].voucher_batch.name, '']);
         this.contentShow = 'exchange2';
         this.couponInfo.code = code;
         this.couponInfo.end_time = end_time;
@@ -568,7 +569,7 @@
       copy() {
         var clipboard = new Clipboard('.btncopy');
         clipboard.on('success', e => {
-          _hmt.push(['_trackEvent','点击复制优惠券', '点击', '优惠券为：'+this.activity_bounty[0].voucher_batch.name, '优惠券码为：'+this.couponInfo.code]);
+          _hmt.push(['_trackEvent','点击复制优惠券', '点击', '复制优惠券为：'+this.activity_bounty[0].voucher_batch.name, '']);
           Toast({
             message: '复制成功',
             position: 'middle',
