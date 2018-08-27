9465488a7bc7553a8a9a996a629b530f183f764d
lester <lester@catchme.com.cn>
2018-08-27 14:03:12 +0800

    修改

--- src/components/index2.vue
+++ src/components/index2.vue
@@ -422,7 +422,7 @@
           <div class="center16-main">
             <img class="imgBg" src="http://res.catchme.com.cn/activity/guide/jieshi.png" alt=""/>
             <h3>
-              累计抓取{{activity_bounty[activity_bounty.length - 1].voucher_batch.value}}次没抓中?<br/>+{{activity_bounty[activity_bounty.length - 1].voucher_batch.description | handleDes}}元拿走!
+              累计抓取{{activity_bounty[activity_bounty.length - 1].voucher_batch.value/info.coin_num}}次没抓中?<br/>+{{activity_bounty[activity_bounty.length - 1].voucher_batch.description | handleDes}}元拿走!
             </h3>
             <p @click="openTip('shuoming')">查看活动说明</p>
             <!--<a class="go-coupon-list" href="javascript:void(0)"-->
