9a2a4c7890fd9337aecce62244027ce203841b36
lester <lester@catchme.com.cn>
2018-09-19 15:24:37 +0800

    修改

--- src/components/task.vue
+++ src/components/task.vue
@@ -162,7 +162,7 @@
             if(prize_bounty && res.prize_bounty>prize_bounty && this.activity_bounty.length > 0){
               _hmt.push(['_trackEvent', '抓中娃娃弹窗', '打开', '进入页面时弹出', '']);
               //弹出抓中娃娃的弹窗
-              this.$emit('openTip','wawaTip');
+              this.$emit('openTip','zhongqiu');
             }
             localStorage.setItem('prize_bounty',res.prize_bounty);
           })
