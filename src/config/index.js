bb899146df9e110cb81d16eb1c19c1f6131f8fad
lester <lester@catchme.com.cn>
2018-09-21 11:29:26 +0800

    修改银行图标

--- src/config/index.js
+++ src/config/index.js
@@ -23,7 +23,7 @@ let CONFIG = {
   scene:1,
   site_version_id:GetCookie('site_version_id') || 0,
   get token(){
-    return   process.env.NODE_ENV === 'development' ? 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zyb250LjV6aHVhLmNuL2FwaS9hdXRoL3Rva2VuIiwiaWF0IjoxNTM3MjM1NDQwLCJleHAiOjE1Mzc0NTE0NDAsIm5iZiI6MTUzNzIzNTQ0MCwianRpIjoicHNtbjkzUTFrQWRmYzBGayIsInN1YiI6Mn0.Lg0_utuWj7M7qb9azonEq7aBlHSeOIIhNTLNlWP2ZxY' : GetCookie('token_')
+    return   process.env.NODE_ENV === 'development' ? 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zyb250LjV6aHVhLmNuL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNTM3NDk3MzMxLCJleHAiOjE1Mzc3MTMzMzEsIm5iZiI6MTUzNzQ5NzMzMSwianRpIjoiQUlFVU5rUGh2VnNmU3lKYyIsInN1YiI6Mn0.sQFoOkyd3-GMZogB78PytXW3oHt1aXbZ6nn1Zo9hnvY' : GetCookie('token_')
   },
   machine_no: getParamByName('machine') || 'CATCH_100024',
   u: function () {
