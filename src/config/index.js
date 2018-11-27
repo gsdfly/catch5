8bf23e67c7e7d4f26334a8fec8c08ad6d523b738
lester <lester@catchme.com.cn>
2018-11-27 10:13:16 +0800

    修改

--- src/config/index.js
+++ src/config/index.js
@@ -25,7 +25,7 @@ let CONFIG = {
   scene:1,
   site_version_id:GetCookie('site_version_id') || 0,
   get token(){
-    return   process.env.NODE_ENV === 'development' ? 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zyb250LjV6aHVhLmNuL2FwaS9hdXRoL3Rva2VuIiwiaWF0IjoxNTQyMjUwNzE3LCJleHAiOjE1NDI0NjY3MTcsIm5iZiI6MTU0MjI1MDcxNywianRpIjoiYzVJMW9LTmlxb0NpRGhvbCIsInN1YiI6Mn0.NysreAKK8bEPoUSi9aNe_YLay39ISE2_oeGYG8LxZcg' : GetCookie('token_')
+    return   process.env.NODE_ENV === 'development' ? 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zyb250LjV6aHVhLmNuL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNTQzMjg0MzMwLCJleHAiOjE1NDM1MDAzMzAsIm5iZiI6MTU0MzI4NDMzMCwianRpIjoiN0RRZ1BWWFE4ZGhQNEVkcyIsInN1YiI6Mn0.gUA0tsjScIxMPG04-WEjKt7Cmv1mj6dtUh-lXOyVOgU' : GetCookie('token_')
   },
   machine_no: getParamByName('machine') || 'CATCH_100024',
   u: function () {
