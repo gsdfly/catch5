e7d5f1df24b90932a5b5577998f5df5ab63ebf72
lester <lester@catchme.com.cn>
2018-12-29 10:25:51 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/sendcoupon10.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'sendcoupon10',
-    assetsPublicPath: process.argv.includes('upload') ? 'http://res.catchme.com.cn/rc/'+package.version:'./',
+    assetsPublicPath: process.argv.includes('upload') ? 'http://res.catchme.com.cn/rc/'+package.version+'/':'./',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
