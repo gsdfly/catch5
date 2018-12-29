94da163bdf7ec317852be4dd1c9820f2207c0296
lester <lester@catchme.com.cn>
2018-12-29 10:26:31 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/yuandan.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'yuandan',
-    assetsPublicPath:process.argv.includes('upload') ? 'http://res.catchme.com.cn/rc/'+package.version:'./',
+    assetsPublicPath:process.argv.includes('upload') ? 'http://res.catchme.com.cn/rc/'+package.version+'/':'./',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
